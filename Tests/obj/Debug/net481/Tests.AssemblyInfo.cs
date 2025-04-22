4a0b89b6d609f214f2fb8b750e90370fb8c49bc1
Soloma <Soloma3040450@Gmail.com>
2025-04-22 14:21:37 +0300

    Модификация доступа

--- Tests/obj/Debug/net481/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net481/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64e8aa29d5cd5c76a959354add441da34600fec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
